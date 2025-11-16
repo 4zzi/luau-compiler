971a1a10fb468d9780f982a968b2ddbdd70ebbbc
zzi <kaze60613@gmail.com>
2025-11-16 13:27:24 +0700

    Update build_solution.bat

--- build_solution.bat
+++ build_solution.bat
@@ -1,8 +1,9 @@
 @echo off
 cls
 
-call "I:\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+call "C:\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 
 msbuild "luaucompiler.sln" /p:Configuration=Release /p:Platform=x64
+
 
 :: start "" "..\build\x64\Release\output\Xeno.exe"
